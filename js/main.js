6445e81f7fcc1872fd1e688339827222e9b095f7
sohamvakani <sohamvakani@gmail.com>
2026-02-23 15:53:01 -0500

    Scatterplot functionality and level 1 wrap

--- js/main.js
+++ js/main.js
@@ -160,3 +160,56 @@ function createHistogramLife(data) {
     .attr("fill", "black")
     .text("Number of Countries");
 }
+
+function createScatterplot(data) {
+  const margin = { top: 20, right: 20, bottom: 50, left: 60 };
+  const width = 1300 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
+
+  const svg = d3
+    .select("#scatterplot")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  //Scales
+  const xScale = d3.scaleLinear().domain([0, 100]).range([0, width]);
+  const yScale = d3.scaleLinear().domain([50, 90]).range([height, 0]);
+
+  //Dots
+  svg
+    .selectAll(".dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "dot")
+    .attr("cx", (d) => xScale(d.internet))
+    .attr("cy", (d) => yScale(d.life))
+    .attr("r", 5);
+
+  //X axis
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xScale))
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", width / 2)
+    .attr("y", 40)
+    .attr("fill", "black")
+    .text("Internet Access (%)");
+
+  //Y axis
+  svg
+    .append("g")
+    .call(d3.axisLeft(yScale))
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -50)
+    .attr("x", -height / 2)
+    .attr("fill", "black")
+    .text("Life Expectancy (years)");
+}
