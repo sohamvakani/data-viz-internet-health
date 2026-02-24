aeac8f8f6b7115d172b47675c580e8cb959e1aaa
sohamvakani <sohamvakani@gmail.com>
2026-02-24 12:00:07 -0500

    small touch ups for level 3 completion

--- js/main.js
+++ js/main.js
@@ -345,7 +345,7 @@ function createScatterplot(data, attribute1Name, attribute2Name) {
 
   const margin = { top: 20, right: 20, bottom: 50, left: 100 };
   const width = 1300 - margin.left - margin.right;
-  const height = 400 - margin.top - margin.bottom;
+  const height = 350 - margin.top - margin.bottom;
 
   const svg = d3
     .select("#scatterplot")
@@ -490,8 +490,8 @@ function createChoropleth(geoData, data, attributeName, containerId) {
   d3.select(containerId).html("");
 
   const margin = { top: 20, right: 20, bottom: 80, left: 20 };
-  const width = 650 - margin.left - margin.right;
-  const height = 450 - margin.top - margin.bottom;
+  const width = 600 - margin.left - margin.right;
+  const height = 500 - margin.top - margin.bottom;
 
   const svgElement = d3
     .select(containerId)
